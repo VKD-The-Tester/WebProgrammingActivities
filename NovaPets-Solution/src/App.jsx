a8a30afade4613ea5768092a87f814848e0a173f
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-02-09 16:59:33 +0200

    First card got added to App.jsx

--- NovaPets-Solution/src/App.jsx
+++ NovaPets-Solution/src/App.jsx
@@ -7,7 +7,13 @@ import Card from "./components/Card";
 function App() {
   return (
     <>
-      <Card stepNumber={1} headingText={}></Card>
+      <Card
+        badgeInput={"Profiles"}
+        headingText={"Real details"}
+        paragraphInfo={
+          "Age, vaccines, energy level, and what the pet is like at home."
+        }
+      ></Card>
       <Card></Card>
     </>
   );
