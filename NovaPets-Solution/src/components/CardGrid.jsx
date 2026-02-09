de7a460dd002e293c69aadfd3a547c3fc6570da6
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-02-10 00:06:23 +0200

    Updated two components with a children property parameter.

---
+++ NovaPets-Solution/src/components/CardGrid.jsx
@@ -0,0 +1,7 @@
+import Card from "./Card";
+
+const CardGrid = ({ children }) => {
+  return <div className="card-grid">{children}</div>;
+};
+
+export default CardGrid;
--- NovaPets-Solution/src/components/HeroActions.jsx
+++ NovaPets-Solution/src/components/HeroActions.jsx
@@ -2,36 +2,8 @@ import { NavLink } from "react-router-dom";
 import "../components-styling/HeroActions.css";
 import Button from "./Button";
 
-const HeroActions = ({
-  buttonClassOne,
-  buttonLinkOne,
-  buttonTextOne,
-  buttonClassTwo,
-  buttonLinkTwo,
-  buttonTextTwo,
-  buttonClassThree = null,
-  buttonLinkThree = null,
-  buttonTextThree = null,
-}) => {
-  return (
-    <div className="hero-actions">
-      <Button
-        className={buttonClassOne}
-        buttonLink={buttonLinkOne}
-        buttonText={buttonTextOne}
-      ></Button>
-      <Button
-        className={buttonClassTwo}
-        buttonLink={buttonLinkTwo}
-        buttonText={buttonTextTwo}
-      ></Button>
-      <Button
-        className={buttonClassThree}
-        buttonLink={buttonLinkThree}
-        buttonText={buttonTextThree}
-      ></Button>
-    </div>
-  );
+const HeroActions = ({ children }) => {
+  return <div className="hero-actions">{children}</div>;
 };
 
 export default HeroActions;
