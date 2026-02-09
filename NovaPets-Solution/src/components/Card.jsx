399c6166a3a7fcbe5760323de32b4824c1105758
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-02-09 16:17:54 +0200

    Card component has been created.

--- NovaPets-Solution/src/components/Card.jsx
+++ NovaPets-Solution/src/components/Card.jsx
@@ -1,5 +1,11 @@
-const Card = () => {
-  return <div>Card</div>;
+const Card = (stepNumber, headingText, paragraphInfo) => {
+  return (
+    <article className="card">
+      <span className="badge">Step {stepNumber}</span>
+      <h3>{headingText}</h3>
+      <p>{paragraphInfo}</p>
+    </article>
+  );
 };
 
 export default Card;
