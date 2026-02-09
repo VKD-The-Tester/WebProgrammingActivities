0a2cd88f772ca48f53c2d0680502d4dcec787443
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-02-09 23:26:51 +0200

    The Hero-Actions component has been redone.

--- NovaPets-Solution/src/components/HeroActions.jsx
+++ NovaPets-Solution/src/components/HeroActions.jsx
@@ -1,15 +1,35 @@
 import { NavLink } from "react-router-dom";
 import "../components-styling/HeroActions.css";
+import Button from "./Button";
 
-const HeroActions = ({ primaryLinkText, primaryLink, LinkText, Link }) => {
+const HeroActions = ({
+  buttonClassOne,
+  buttonLinkOne,
+  buttonTextOne,
+  buttonClassTwo,
+  buttonLinkTwo,
+  buttonTextTwo,
+  buttonClassThree = null,
+  buttonLinkThree = null,
+  buttonTextThree = null,
+}) => {
   return (
     <div className="hero-actions">
-      <NavLink className="btn btn-primary" Link={primaryLink}>
-        {primaryLinkText}
-      </NavLink>
-      <NavLink className="btn" Link={Link}>
-        {LinkText}
-      </NavLink>
+      <Button
+        className={buttonClassOne}
+        buttonLink={buttonLinkOne}
+        buttonText={buttonTextOne}
+      ></Button>
+      <Button
+        className={buttonClassTwo}
+        buttonLink={buttonLinkTwo}
+        buttonText={buttonTextTwo}
+      ></Button>
+      <Button
+        className={buttonClassThree}
+        buttonLink={buttonLinkThree}
+        buttonText={buttonTextThree}
+      ></Button>
     </div>
   );
 };
