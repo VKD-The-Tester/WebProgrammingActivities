a25b62f2e9a57d178a3da471aa2c072836a6404c
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-02-10 00:17:44 +0200

    The StatGrid component has been created.

---
+++ NovaPets-Solution/src/components/StatGrid.jsx
@@ -0,0 +1,8 @@
+import "../components-styling/StatGrid.css";
+import Stat from "./Stat";
+
+const StatGrid = ({ children }) => {
+  return <div className="stat-grid">{children}</div>;
+};
+
+export default StatGrid;
