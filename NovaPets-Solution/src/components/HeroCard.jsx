0ba781b3f6cc7cc7e91de9a7fbe89445ad717a6c
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-02-10 00:10:00 +0200

    The Hero Card will be set to receive children.

--- NovaPets-Solution/src/components/HeroCard.jsx
+++ NovaPets-Solution/src/components/HeroCard.jsx
@@ -2,38 +2,8 @@ import "../components-styling/HeroCard.css";
 import HeroActions from "./HeroActions";
 import Kicker from "./Kicker";
 
-const HeroCard = ({
-  kickerText,
-  headingText,
-  paragraphInfo,
-  buttonClassOne,
-  buttonLinkOne,
-  buttonTextOne,
-  buttonClassTwo,
-  buttonLinkTwo,
-  buttonTextTwo,
-  buttonClassThree = null,
-  buttonLinkThree = null,
-  buttonTextThree = null,
-}) => {
-  return (
-    <>
-      <Kicker kickerText={kickerText}></Kicker>
-      <h1>{headingText}</h1>
-      <p>{paragraphInfo}</p>
-      <HeroActions
-        buttonClassOne={buttonClassOne}
-        buttonLinkOne={buttonLinkOne}
-        buttonTextOne={buttonTextOne}
-        buttonClassTwo={buttonClassTwo}
-        buttonLinkTwo={buttonLinkTwo}
-        buttonTextTwo={buttonClassTwo}
-        buttonClassThree={buttonClassThree}
-        buttonLinkThree={buttonLinkThree}
-        buttonTextThree={buttonTextThree}
-      ></HeroActions>
-    </>
-  );
+const HeroCard = ({ children }) => {
+  return <div className="hero-card">{children}</div>;
 };
 
 export default HeroCard;
