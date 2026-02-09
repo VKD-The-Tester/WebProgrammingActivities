ea281fd4567cb06bcec2e1013aab4a036ef48eeb
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-02-09 20:47:29 +0200

    The Hero-Actions css file got imported to the component.

--- NovaPets-Solution/src/components/HeroActions.jsx
+++ NovaPets-Solution/src/components/HeroActions.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import "../components-styling/HeroActions.css";
 
 const HeroActions = ({ primaryLinkText, primaryLink, LinkText, Link }) => {
   return (
