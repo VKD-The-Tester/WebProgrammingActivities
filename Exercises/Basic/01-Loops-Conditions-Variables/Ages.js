adaef7816c1515ecc42a537155476073792aafde
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-02-10 16:47:05 +0200

    I fixed the block or multi-line comment in the Age.js file.

--- Exercises/Basic/01-Loops-Conditions-Variables/Ages.js
+++ Exercises/Basic/01-Loops-Conditions-Variables/Ages.js
@@ -1,6 +1,10 @@
 const age = Number(prompt("Enter in a person's age: "));
 
-/*  To enforce the boundaries I made use of the logical and operator (&&) and to satisfy the inclusive values requirement, I made sure that my conditional expressions used the greater than or equal to (>=) and less than or equal to (<=) conditions. This way we have an inclusive restricted range to work with when we are classifying what age group a person is associated with. */
+/*  
+To enforce the boundaries I made use of the logical and operator (&&) and to satisfy the inclusive values requirement, 
+I made sure that my conditional expressions used the greater than or equal to (>=) and less than or equal to (<=) conditions. 
+This way we have an inclusive restricted range to work with when we are classifying what age group a person is associated with. 
+*/
 
 if (age >= 0 && age <= 2) {
   console.log("baby");
